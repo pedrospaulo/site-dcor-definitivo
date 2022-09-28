158da1edacbfe6dd52d98a0c5ca5ce61a67a5286
Pedro Sousa <pedropauloassis@live.com>
2022-09-28 15:14:25 -0300

    mais

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,45 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-
-
 import { HomeFourComponent } from './components/pages/home-four/home-four.component';
-
-
-
-
 import { ContactComponent } from './components/pages/contact/contact.component';
-
 import { ComingSoonComponent } from './components/pages/coming-soon/coming-soon.component';
 import { ErrorComponent } from './components/pages/error/error.component';
 import { PrivacyPolicyComponent } from './components/pages/privacy-policy/privacy-policy.component';
 import { TermsConditionsComponent } from './components/pages/terms-conditions/terms-conditions.component';
-
 import { TeamComponent } from './components/pages/team/team.component';
 import { ProjectsDetailsComponent } from './components/pages/projects-details/projects-details.component';
-
 import { ServicesDetailsComponent } from './components/pages/services-details/services-details.component';
 import { ServicesTwoComponent } from './components/pages/services-two/services-two.component';
-
 import { AboutComponent } from './components/pages/about/about.component';
 
 const routes: Routes = [
 
-    {path: '', component: HomeFourComponent},
-
-    {path: 'about', component: AboutComponent},
-
+    {path: '', component: HomeFourComponent, data: { titulo: 'Inicio' }},
+    {path: 'about', component: AboutComponent, data: { titulo: 'Inicio' }},
     {path: 'services-two', component: ServicesTwoComponent},
     {path: 'services-details', component: ServicesDetailsComponent},
-
     {path: 'projects-details', component: ProjectsDetailsComponent},
     {path: 'team', component: TeamComponent},
-
     {path: 'terms-conditions', component: TermsConditionsComponent},
     {path: 'privacy-policy', component: PrivacyPolicyComponent},
     {path: 'error', component: ErrorComponent},
     {path: 'coming-soon', component: ComingSoonComponent},
-
     {path: 'contact', component: ContactComponent},
     // Here add new pages component
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PreloaderComponent } from './components/layouts/preloader/preloader.component';
 import { FooterComponent } from './components/layouts/footer/footer.component';
-
 import { HomeFourComponent } from './components/pages/home-four/home-four.component';
-
 import { NavbarOneComponent } from './components/layouts/navbar-one/navbar-one.component';
-
 import { AboutComponent } from './components/pages/about/about.component';
-
 import { ServicesTwoComponent } from './components/pages/services-two/services-two.component';
 import { ServicesDetailsComponent } from './components/pages/services-details/services-details.component';
-
 import { ProjectsDetailsComponent } from './components/pages/projects-details/projects-details.component';
 import { ContactComponent } from './components/pages/contact/contact.component';
-
 import { TeamComponent } from './components/pages/team/team.component';
-
 import { TermsConditionsComponent } from './components/pages/terms-conditions/terms-conditions.component';
 import { PrivacyPolicyComponent } from './components/pages/privacy-policy/privacy-policy.component';
 import { ErrorComponent } from './components/pages/error/error.component';
@@ -31,25 +23,17 @@ import { ComingSoonComponent } from './components/pages/coming-soon/coming-soon.
     AppComponent,
     PreloaderComponent,
     FooterComponent,
-
     HomeFourComponent,
-
     NavbarOneComponent,
-
     AboutComponent,
-
     ServicesTwoComponent,
     ServicesDetailsComponent,
-
     ProjectsDetailsComponent,
     ContactComponent,
-
     TeamComponent,
-
     TermsConditionsComponent,
     PrivacyPolicyComponent,
     ErrorComponent,
-
     ComingSoonComponent
   ],
   imports: [
